b50d9a4e08242d1fc279b31d9cfea1c51d2f63e9
Callum Anning <CallumAnning@hotmail.co.uk>
2024-04-23 16:59:38 +0100

    Refactoring

--- PlayWav/Main.cpp
+++ PlayWav/Main.cpp
@@ -1,196 +1,163 @@
 #include <iostream>
 #include <xaudio2.h>
 #include <Windows.h>
+#include <fstream>
+#include <vector>
+#include <string>
 
-#ifndef _XBOX //Little-Endian
-#define fourccRIFF 'FFIR'
-#define fourccDATA 'atad'
-#define fourccFMT ' tmf'
-#define fourccWAVE 'EVAW'
-#define fourccXWMA 'AMWX'
-#define fourccDPDS 'sdpd'
-#endif
-
-HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD& dwChunkSize, DWORD& dwChunkDataPosition)
+enum class FourCC
 {
-    HRESULT hr = S_OK;
-    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, 0, NULL, FILE_BEGIN))
-        return HRESULT_FROM_WIN32(GetLastError());
+	UNKNOWN = 0,
+	RIFF = 'FFIR',
+	FMT = ' tmf',
+	DATA = 'atad',
+	WAVE = 'EVAW',
+};
 
-    DWORD dwChunkType;
-    DWORD dwChunkDataSize;
-    DWORD dwRIFFDataSize = 0;
-    DWORD dwFileType;
-    DWORD bytesRead = 0;
-    DWORD dwOffset = 0;
+void WaveScanType(std::ifstream& file)
+{
+	file.seekg(0, std::ios::beg);
 
-    while (hr == S_OK)
-    {
-        DWORD dwRead;
-        if (0 == ReadFile(hFile, &dwChunkType, sizeof(DWORD), &dwRead, NULL))
-            hr = HRESULT_FROM_WIN32(GetLastError());
+	while (true)
+	{
+		FourCC chunk_type;
+		file.read(reinterpret_cast<char*>(&chunk_type), sizeof(FourCC));
 
-        if (0 == ReadFile(hFile, &dwChunkDataSize, sizeof(DWORD), &dwRead, NULL))
-            hr = HRESULT_FROM_WIN32(GetLastError());
+		if (chunk_type == FourCC::RIFF)
+		{
+			int chunk_size = 0;
+			file.read(reinterpret_cast<char*>(&chunk_size), sizeof(chunk_size));
 
-        switch (dwChunkType)
-        {
-            case fourccRIFF:
-                dwRIFFDataSize = dwChunkDataSize;
-                dwChunkDataSize = 4;
-                if (0 == ReadFile(hFile, &dwFileType, sizeof(DWORD), &dwRead, NULL))
-                    hr = HRESULT_FROM_WIN32(GetLastError());
-                break;
+			FourCC file_type;
+			file.read(reinterpret_cast<char*>(&file_type), sizeof(FourCC));
 
-            default:
-                if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, dwChunkDataSize, NULL, FILE_CURRENT))
-                    return HRESULT_FROM_WIN32(GetLastError());
-        }
+			if (file_type != FourCC::WAVE)
+			{
+				std::cerr << "Not a WAVE file\n";
+				return;
+			}
 
-        dwOffset += sizeof(DWORD) * 2;
-
-        if (dwChunkType == fourcc)
-        {
-            dwChunkSize = dwChunkDataSize;
-            dwChunkDataPosition = dwOffset;
-            return S_OK;
-        }
-
-        dwOffset += dwChunkDataSize;
-
-        if (bytesRead >= dwRIFFDataSize) return S_FALSE;
-
-    }
-
-    return S_OK;
-
+			return;
+		}
+	}
 }
 
-HRESULT ReadChunkData(HANDLE hFile, void* buffer, DWORD buffersize, DWORD bufferoffset)
+WAVEFORMATEXTENSIBLE WaveScanWFX(std::ifstream& file)
 {
-    HRESULT hr = S_OK;
-    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, bufferoffset, NULL, FILE_BEGIN))
-        return HRESULT_FROM_WIN32(GetLastError());
-    DWORD dwRead;
-    if (0 == ReadFile(hFile, buffer, buffersize, &dwRead, NULL))
-        hr = HRESULT_FROM_WIN32(GetLastError());
-    return hr;
+	file.seekg(0, std::ios::beg);
+
+	while (true)
+	{
+		FourCC chunk_type;
+		file.read(reinterpret_cast<char*>(&chunk_type), sizeof(FourCC));
+
+		if (chunk_type == FourCC::FMT)
+		{
+			int chunk_size = 0;
+			file.read(reinterpret_cast<char*>(&chunk_size), sizeof(chunk_size));
+
+			WAVEFORMATEXTENSIBLE wfx;
+			file.read(reinterpret_cast<char*>(&wfx), sizeof(WAVEFORMATEXTENSIBLE));
+
+			return wfx;
+		}
+	}
 }
 
-static HRESULT PlayWav(IXAudio2* audio)
+std::vector<BYTE> WaveScanBuffer(std::ifstream& file)
 {
-    WAVEFORMATEXTENSIBLE wfx = { 0 };
-    XAUDIO2_BUFFER buffer = { 0 };
+	file.seekg(0, std::ios::beg);
 
-    LPCWSTR strFileName = L"C:/Users/Callum/Downloads/gun_44mag_11.wav";
-    // LPCWSTR strFileName = L"gun_44mag_11.wav";
+	while (true)
+	{
+		FourCC chunk_type;
+		file.read(reinterpret_cast<char*>(&chunk_type), sizeof(FourCC));
 
-    /*std::ifstream file3("C:/Users/Callum/Downloads/gun_44mag_11.wav", std::ios::binary | std::ios::in);
-    bool is_open = file3.is_open();*/
+		if (chunk_type == FourCC::DATA)
+		{
+			int chunk_size = 0;
+			file.read(reinterpret_cast<char*>(&chunk_size), sizeof(chunk_size));
 
-    // Open the file
-    HANDLE hFile = CreateFile(
-        strFileName,
-        GENERIC_READ,
-        FILE_SHARE_READ,
-        NULL,
-        OPEN_EXISTING,
-        0,
-        NULL);
+			std::vector<BYTE> buffer;
+			buffer.resize(chunk_size);
 
-    if (INVALID_HANDLE_VALUE == hFile)
-    {
-        return HRESULT_FROM_WIN32(GetLastError());
-    }
+			file.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
+			return buffer;
+		}
+	}
+}
 
-    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, 0, NULL, FILE_BEGIN))
-    {
-        return HRESULT_FROM_WIN32(GetLastError());
-    }
+static HRESULT PlayWav(IXAudio2* audio, LPCWSTR fileName)
+{
+	std::ifstream file(fileName, std::ios::in | std::ios::binary);
+	file.seekg(0, std::ios::beg);
 
-    DWORD dwChunkSize;
-    DWORD dwChunkPosition;
-    //check the file type, should be fourccWAVE or 'XWMA'
-    FindChunk(hFile, fourccRIFF, dwChunkSize, dwChunkPosition);
-    DWORD filetype;
-    ReadChunkData(hFile, &filetype, sizeof(DWORD), dwChunkPosition);
-    if (filetype != fourccWAVE)
-        return S_FALSE;
+	WaveScanType(file);
+	WAVEFORMATEXTENSIBLE wfx1 = WaveScanWFX(file);
+	std::vector<BYTE> buffer23 = WaveScanBuffer(file);
 
-    FindChunk(hFile, fourccFMT, dwChunkSize, dwChunkPosition);
-    ReadChunkData(hFile, &wfx, dwChunkSize, dwChunkPosition);
+	// Create audio source buffer
+	XAUDIO2_BUFFER buffer = { 0 };
+	buffer.AudioBytes = buffer23.size();  //size of the audio buffer in bytes
+	buffer.pAudioData = buffer23.data();  //buffer containing audio data
+	buffer.Flags = XAUDIO2_END_OF_STREAM; // tell the source voice not to expect any data after this buffer
 
-    //fill out the audio data buffer with the contents of the fourccDATA chunk
-    FindChunk(hFile, fourccDATA, dwChunkSize, dwChunkPosition);
-    BYTE* pDataBuffer = new BYTE[dwChunkSize];
-    ReadChunkData(hFile, pDataBuffer, dwChunkSize, dwChunkPosition);
+	// Create source voice
+	IXAudio2SourceVoice* pSourceVoice = nullptr;
+	audio->CreateSourceVoice(&pSourceVoice, (WAVEFORMATEX*)&wfx1);
+	pSourceVoice->SubmitSourceBuffer(&buffer);
+	pSourceVoice->Start(0);
 
-    buffer.AudioBytes = dwChunkSize;  //size of the audio buffer in bytes
-    buffer.pAudioData = pDataBuffer;  //buffer containing audio data
-    buffer.Flags = XAUDIO2_END_OF_STREAM; // tell the source voice not to expect any data after this buffer
+	// Loop until sound is finished
+	while (true)
+	{
+		XAUDIO2_VOICE_STATE state;
+		pSourceVoice->GetState(&state);
+		if (state.BuffersQueued == 0)
+		{
+			break;
+		}
 
-    IXAudio2SourceVoice* pSourceVoice = nullptr;
+		Sleep(100);
+	}
 
-    HRESULT hr;
-    hr = audio->CreateSourceVoice(&pSourceVoice, (WAVEFORMATEX*)&wfx);
-    if (FAILED(hr))
-    {
-        std::cerr << "Failed to create source voice" << std::endl;
-        return hr;
-    }
-
-    hr = pSourceVoice->SubmitSourceBuffer(&buffer);
-    if (FAILED(hr))
-    {
-        std::cerr << "Failed to submit source buffer" << std::endl;
-        pSourceVoice->DestroyVoice();
-        return hr;
-    }
-
-    hr = pSourceVoice->Start(0);
-    if (FAILED(hr))
-    {
-        std::cerr << "Failed to start playback" << std::endl;
-        pSourceVoice->DestroyVoice();
-        return hr;
-    }
+	return S_OK;
 }
 
 int main(int args, char** argv)
 {
-    // Initialize COM
-    if (CoInitializeEx(nullptr, COINIT_MULTITHREADED) != S_OK)
-    {
-        std::cerr << "CoInitializeEx failed\n";
-        return -1;
-    }
+	// Initialize COM
+	if (CoInitializeEx(nullptr, COINIT_MULTITHREADED) != S_OK)
+	{
+		std::cerr << "CoInitializeEx failed\n";
+		return -1;
+	}
 
-    // Initialize XAudio2 - XAudio2 can also be wrapped in a ComPtr
-    IXAudio2* xaudio2 = nullptr;
-    if (XAudio2Create(&xaudio2) != S_OK)
-    {
-        std::cerr << "XAudio2Create failed\n";
-        CoUninitialize();
-        return -1;
-    }
+	// Initialize XAudio2 - XAudio2 can also be wrapped in a ComPtr
+	IXAudio2* xaudio2 = nullptr;
+	if (XAudio2Create(&xaudio2) != S_OK)
+	{
+		std::cerr << "XAudio2Create failed\n";
+		CoUninitialize();
+		return -1;
+	}
 
-    // Create mastering voice
-    IXAudio2MasteringVoice* master_voice = nullptr;
-    if (xaudio2->CreateMasteringVoice(&master_voice) != S_OK)
-    {
-        std::cerr << "CreateMasteringVoice failed\n";
-        CoUninitialize();
-        return -1;
-    }
+	// Create mastering voice
+	IXAudio2MasteringVoice* master_voice = nullptr;
+	if (xaudio2->CreateMasteringVoice(&master_voice) != S_OK)
+	{
+		std::cerr << "CreateMasteringVoice failed\n";
+		CoUninitialize();
+		return -1;
+	}
 
 	std::cout << "XAudio2 successfully initialized\n";
-    PlayWav(xaudio2);
-    Sleep(2000);
+	PlayWav(xaudio2, L"C:/Users/Callum/Downloads/gun_44mag_11.wav");
 
-    // Cleanup
-    master_voice->DestroyVoice();
-    xaudio2->Release();
-    CoUninitialize();
+	// Cleanup
+	master_voice->DestroyVoice();
+	xaudio2->Release();
+	CoUninitialize();
 
 	return 0;
 }
